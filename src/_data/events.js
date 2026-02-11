5d23373159f565155e6f517acd8f5c27f758f327
FunctionDJ <34871211+FunctionDJ@users.noreply.github.com>
2026-02-11 17:06:59 +0100

    sortiere events nach datum

--- src/_data/events.js
+++ src/_data/events.js
@@ -1,6 +1,13 @@
 import "dotenv/config";
 import { getSpreadsheet } from "../_lib/google-sheets.js";
 
+// Parse date string in format "DD.MM.YYYY"
+const parseDate = (dateStr) => {
+	if (!dateStr) return new Date(0);
+	const [day, month, year] = dateStr.split(".");
+	return new Date(year, month - 1, day);
+};
+
 /**
  * The returned data will be available as `events` in templates
  */
@@ -9,8 +16,11 @@ export default async function () {
 
 	console.log("events", events);
 
-	return events.map((event) => ({
-		...event,
-		Tags: event.Tags.split(",").map((tag) => tag.trim()),
-	}));
+	return events
+		.map((event) => ({
+			...event,
+			Tags: event.Tags.split(",").map((tag) => tag.trim()),
+			parsedDate: parseDate(event.Datum),
+		}))
+		.toSorted((a, b) => a.parsedDate - b.parsedDate);
 }
